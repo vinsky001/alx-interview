3b3910a7ace54ec0e8f185d4f514e994a926ae3f
vinsky001 <ekibet544@gmail.com>
2023-09-28 13:37:55 -0700

    Pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+"""
+Pascal's Triangle
+"""
+
+
+def pascal_triangle(n):
+    """Returns a list of integers representing the pascal's triangle"""
+    if n <= 0:
+        return []
+    a = [[] for i in range(n)]
+    for i in range(n):
+        for j in range(i + 1):
+            if j < i:
+                if j == 0:
+                    a[i].append(1)
+                else:
+                    a[i].append(a[i - 1][j] + a[i - 1][j - 1])
+            elif j == i:
+                a[i].append(1)
+    return a

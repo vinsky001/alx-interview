4d623abd8cff4e75fba7aa9acd4d7062a6d3bca6
vinsky001 <ekibet544@gmail.com>
2023-10-12 19:28:06 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -15,7 +15,7 @@ exactly n H characters in the file.
 def minOperations(n):
     """clculates the fewest number of operations needed to
     result in exactly n H characters in the file."""
-     if n <= 1:
+    if n <= 1:
         return 0
 
     i = 2

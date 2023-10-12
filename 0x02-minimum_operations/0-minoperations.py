b504b65f256a5f4954b23087ae7d3b46448a2f13
vinsky001 <ekibet544@gmail.com>
2023-10-12 19:22:14 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -15,13 +15,27 @@ exactly n H characters in the file.
 def minOperations(n):
     """clculates the fewest number of operations needed to
     result in exactly n H characters in the file."""
-    operations = 0
-    chars = 'h'
-    buffer = ''
-    while len(chars) < n:
-        if n % len(chars) == 0:
-            buffer = chars
-            operations += 1
-        chars += buffer
-        operations += 1
-    return operations
+     if n <= 1:
+        return 0
+
+    i = 2
+    count = 0
+
+    while i <= n:
+        if n % i == 0:
+            count += i
+            n /= i
+        else:
+            i += 1
+
+    return count
+
+
+if __name__ == '__main__':
+    n = 4
+    print("Min # of operations to reach {} characters: {}"
+          .format(n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} characters: {}"
+          .format(n, minOperations(n)))
